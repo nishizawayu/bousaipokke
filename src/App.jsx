0f0b31773e2107a42aff8c4d50c699fab8595913
YuyaNishizawa <yuya1106kmey@gmail.com>
2023-03-28 19:31:33 +0900

    firstcomit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = ()=> {
+  
+  return (
+    <>
+      <p className='text-3xl font-bold underline'>あいうえお</p>
+      
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
