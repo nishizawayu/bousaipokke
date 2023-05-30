8393db9a1b7b177560ede17aecc9edeed583d5be
nishizawayuya <22aw0128@jec.ac.jp>
2023-05-30 13:34:43 +0900

    微調整

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './Styles.css'
+import './styles.css'
 import Top from './Top'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
